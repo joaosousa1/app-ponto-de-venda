3080fe1b28a80f99548bd60f5d64e03c6eaad0d0
João Sousa <js.jcps@gmail.com>
2021-08-05 00:33:08 +0100

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,7 +1,7 @@
 //copyright João Sousa 2021
 var cores = ['#FFC312', '#C4E538', '#12CBC4', '#FDA7DF', '#ED4C67', '#F79F1F', '#A3CB38', '#1289A7', '#D980FA', '#B53471', '#EE5A24', '#009432', '#0652DD', '#9980FA', '#833471', '#EA2027', '#006266', '#1B1464', '#5758BB', '#6F1E51'] //tem de ser 20
 
-var taxaIva = {'normal': 23, 'intermadia': 12, 'reduzida': 6}
+var taxaIva = {'normal': 23, 'intermedia': 12, 'reduzida': 6}
 
 var produtos = [
     {
